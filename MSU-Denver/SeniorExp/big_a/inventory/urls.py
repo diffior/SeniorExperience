861c37cbc7bea88aa9fd7d8d9e39e0ba4dbb5dc0
Christian <ccampb86@msudenver.edu>
2024-04-02 14:29:20 -0600

    Remove url path

--- MSU-Denver/SeniorExp/big_a/inventory/urls.py
+++ MSU-Denver/SeniorExp/big_a/inventory/urls.py
@@ -7,7 +7,6 @@ app_name = 'inventory'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('', include('inventory.urls')), this breaks the app
     path('inventory/', views.inventory_list, name='inventory-list'),
     path('inventory/<int:pk>/', views.inventory_detail, name='inventory-detail'),
     path('', views.inventory_list, name='inventory-list'),
