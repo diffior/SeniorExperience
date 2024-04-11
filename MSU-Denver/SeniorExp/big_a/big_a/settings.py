17e14cc2e1894394fd9d3efb6a907e06ee55ce59
Josh <hlee43@msudenver.edu>
2024-04-11 15:34:24 -0600

    update to configure to heroku

--- MSU-Denver/SeniorExp/big_a/big_a/settings.py
+++ MSU-Denver/SeniorExp/big_a/big_a/settings.py
@@ -31,7 +31,7 @@ CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:8081',
     'http://localhost:3000',
-    # 'https://joshhklee.github.io/',
+    'https://joshhklee.github.io',
 )
 
 
